10f31431a5fdd28d8e224e1ed7d627c045b452c2
Jeremy Roberts <jaroberts@ksu.edu>
2018-12-14 00:23:38 -0600

    Syncing all files, updated instructions

--- inputs/assembly/assembly.py
+++ inputs/assembly/assembly.py
@@ -8,10 +8,7 @@ import h5py
 import numpy as np
 
   
-###########################################################################
-# MATERIALS
-###########################################################################
-
+#%% Materials
 uo2 = openmc.Material(1, "uo2")
 uo2.add_nuclide('U235', 0.04)
 uo2.add_nuclide('U238', 0.96)
@@ -32,12 +29,8 @@ mats = openmc.Materials()
 mats += [uo2, zirconium, h2o]
 mats.export_to_xml()
        
-###########################################################################
-# GEOMETRY   
-###########################################################################
-
+#%% Geometry
 pitch = 1.26
-
 boundary = 'reflective'
 left   = openmc.XPlane(x0=-pitch*2, boundary_type=boundary)
 right  = openmc.XPlane(x0=pitch*2, boundary_type=boundary)
@@ -90,22 +83,7 @@ root.add_cells([lattice_cell])
 geom = openmc.Geometry(root)
 geom.export_to_xml()
 
-###########################################################################
-# SETTINGS
-###########################################################################
-
-settings = openmc.Settings()
-
-# Create an initial uniform spatial source distribution over fissionable zones
-bounds = [-2*pitch, -2*pitch, -1, 2*pitch, 2*pitch, 1]
-uniform_dist = openmc.stats.Box(bounds[:3], bounds[3:], only_fissionable=True)
-settings.source = openmc.source.Source(space=uniform_dist)
-
-settings.batches = 100
-settings.inactive = 10
-settings.particles = 1000
-settings.export_to_xml()
-
+# plot
 p = openmc.Plot()
 p.filename = 'pinplot'
 p.width = (4*1.26, 4*1.26)
@@ -116,89 +94,40 @@ plots.export_to_xml()
 openmc.plot_geometry()
 openmc.plot_inline(p)
 
-# This mesh is identical to the tally mesh but must be repeated for
-# use as an entropy mesh.  Note, the third dimension is needed for
-# the entropy mesh.
-entropy_mesh = openmc.Mesh(mesh_id=1)
-entropy_mesh.type = 'regular'
-entropy_mesh.dimension = [4, 4, 1]
-entropy_mesh.lower_left = [-2*pitch, -2*pitch, -100000]
-entropy_mesh.upper_right = [2*pitch, 2*pitch, 100000]
-settings.entropy_mesh = entropy_mesh
-settings.export_to_xml()
 
-###########################################################################
-# TALLIES
-###########################################################################
+#%% Tallies
 
-# cell
-cell_filter = openmc.CellFilter([fuel.id, coolant.id])
+# energy bins
 energy_filter = openmc.EnergyFilter([0.0, 0.625, 2.0e7])
 
-# tally mesh
-mesh = openmc.Mesh(mesh_id=2)
-mesh.type = 'regular'
-mesh.dimension = [4, 4]
-mesh.lower_left = [-2*pitch, -2*pitch]
-mesh.width = [pitch, pitch]
-mesh_filter = openmc.MeshFilter(mesh)
-
-# tallies
+# tally flux over whole geometry
 flux_tally = openmc.Tally(tally_id=1)
-flux_tally.filters = [cell_filter, energy_filter]
+flux_tally.filters = [energy_filter]
 flux_tally.scores = ['flux']
 
+
+# tally mesh
+mesh = openmc.Mesh(mesh_id=1)
+mesh.type = 'regular'
+mesh.dimension = [4, 4, 1]
+mesh.lower_left = [-2*pitch, -2*pitch, -1e7]
+mesh.width = [pitch, pitch, 2e7]
+mesh_filter = openmc.MeshFilter(mesh)
 mesh_tally = openmc.Tally(tally_id=2)
 mesh_tally.filters = [mesh_filter]
-mesh_tally.scores = ['absorption', 'fission']
+mesh_tally.scores = ['fission']
 
+# export to file
 tallies_file = openmc.Tallies([flux_tally, mesh_tally])
-
-# MULTIGROUP CROSS SECTIONS
-groups = mgxs.EnergyGroups()
-groups.group_edges = np.array([0., 0.625, 2e7])
-total = mgxs.TotalXS(domain=root, domain_type='universe', groups=groups)
-absorption = mgxs.AbsorptionXS(domain=root, domain_type='universe', groups=groups)
-scatteringM = mgxs.ScatterMatrixXS(domain=root, domain_type='universe', 
-                                   groups=groups)
-scatteringM.correction = None
-scatteringM.formulation = 'consistent'
-scattering = mgxs.ScatterXS(domain=root, domain_type='universe', groups=groups)
-
-fission = mgxs.FissionXS(domain=root, groups=groups, nu=True)
-chi = mgxs.Chi(domain=root, groups=groups)
-tallies_file += total.tallies.values()
-tallies_file += absorption.tallies.values()
-tallies_file += scattering.tallies.values()
-tallies_file += scatteringM.tallies.values()
-
-tallies_file += fission.tallies.values()
-tallies_file += chi.tallies.values()
 tallies_file.export_to_xml()
 
-openmc.run(threads=4)
+#%% Settings
+settings = openmc.Settings()
+settings.batches = 100
+settings.inactive = 10
+settings.particles = 1000
+settings.export_to_xml()
 
-sp = openmc.StatePoint('statepoint.100.h5')
-total.load_from_statepoint(sp)
-absorption.load_from_statepoint(sp)
-scattering.load_from_statepoint(sp)
-scatteringM.load_from_statepoint(sp)
-fission.load_from_statepoint(sp)
-chi.load_from_statepoint(sp)
-
-# build cross section operators
-T = np.array(total.get_pandas_dataframe()['mean'])
-A = np.array(absorption.get_pandas_dataframe()['mean'])
-F = np.array(fission.get_pandas_dataframe()['mean'])
-ST = np.array(scattering.get_pandas_dataframe()['mean'])
-
-X = np.array(chi.get_pandas_dataframe()['mean'])
-df = scatteringM.get_pandas_dataframe()
-S = np.zeros((2, 2))
-S[:, 0] = df[df['group in']==1]['mean']
-S[:, 1] = df[df['group in']==2]['mean']
- 
-# (T-S)phi = (1/k)*X'F  
-M = np.linalg.solve(np.diag(T)-S, np.outer(X, F))
-e, v = np.linalg.eig(M)
+#%% Run and view
+openmc.run(threads=1)
 
